9aebaee17c236ba718b0f963787acccabf7d9ba2
Yuriy <user@Mac-mini-administrator-91.local>
2013-09-06 19:13:12 +0400

    123

---
+++ Apple/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by user on 06.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Apple/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/Test2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test2
+//
+//  Created by user on 06.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
