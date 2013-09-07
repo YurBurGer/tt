876e23b1de98bac2e8e4339dd41f4b62068987ad
Yuriy <user@Mac-mini-administrator-91.local>
2013-09-07 17:14:31 +0400

    tests

--- Task2/Task2/ViewController.h
+++ Task2/Task2/ViewController.h
@@ -9,5 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *L1;
+- (IBAction)B1:(id)sender;
+@property (weak, nonatomic) IBOutlet UISwitch *S1;
+- (IBAction)S1:(id)sender;
+@property (weak, nonatomic) IBOutlet UIImageView *I1;
+- (IBAction)B2:(id)sender;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *V1;
+- (IBAction)V1:(id)sender;
+@property (weak, nonatomic) IBOutlet UISlider *Sl1;
+- (IBAction)Sl1:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *L2;
+- (IBAction)Inc1:(id)sender;
+
+
 
 @end
--- Task2/Task2/ViewController.m
+++ Task2/Task2/ViewController.m
@@ -26,4 +26,55 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)B1:(id)sender {
+    if([self.L1.text isEqual:@"Label"]){
+        if (self.S1.isOn) {
+            self.L1.text=@"NotLabel";
+        } else {
+            self.L1.text=@"AbsolutelyNotLabel";
+        }
+    }else{
+        self.L1.text=@"Label";
+    }
+}
+
+- (IBAction)S1:(id)sender {
+    if(![self.L1.text isEqual:@"Label"]){
+        if (self.S1.isOn) {
+            self.L1.text=@"NotLabel";
+        } else {
+            self.L1.text=@"AbsolutelyNotLabel";
+        }
+    }
+}
+
+- (IBAction)B2:(id)sender {
+    UIImage *img=[UIImage  imageNamed:@"item6.png"];
+    self.I1.image=img;
+}
+
+- (IBAction)V1:(id)sender {
+    UIImage *img;
+    switch (self.V1.selectedSegmentIndex) {
+        case 0:
+            img=[UIImage  imageNamed:@"item2.png"];
+            break;
+        case 1:
+            img=[UIImage  imageNamed:@"item3.png"];
+            break;
+        case 2:
+            img=[UIImage  imageNamed:@"item4.png"];
+            break;
+        default:
+            img=[UIImage  imageNamed:@"item6.png"];
+            break;
+    }
+    self.I1.image=img;
+}
+- (IBAction)Sl1:(id)sender {
+    self.L2.text=[NSString stringWithFormat:@"%g", self.Sl1.value];
+}
+- (IBAction)Inc1:(id)sender {
+    
+}
 @end
