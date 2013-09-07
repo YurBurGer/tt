29a4a316728393e39ade137f04db05c1db9f71b7
Yuriy <user@Mac-mini-administrator-91.local>
2013-09-07 15:43:22 +0400

    13231

---
+++ Task2/Task2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Task2
+//
+//  Created by user on 07.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
