b85cf436e8c9d7c680a8cdf03a15d69f1ebeba06
Yuriy <user@Mac-mini-administrator-91.local>
2013-09-11 16:20:45 +0400

    New Project

---
+++ Task3/Task3Tests/Task3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Task3Tests.m
+//  Task3Tests
+//
+//  Created by user on 11.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Task3Tests : XCTestCase
+
+@end
+
+@implementation Task3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
