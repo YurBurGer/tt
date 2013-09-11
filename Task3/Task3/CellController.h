3bec3b2bd586516ebefb529db25ccc80a1953b55
Yuriy <user@Mac-mini-administrator-91.local>
2013-09-11 16:34:08 +0400

    More commit

---
+++ Task3/Task3/CellController.h
@@ -0,0 +1,13 @@
+//
+//  CellController.h
+//  Task3
+//
+//  Created by user on 11.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellController : UIViewController
+
+@end
---
+++ Task3/Task3/CellController.m
@@ -0,0 +1,38 @@
+//
+//  CellController.m
+//  Task3
+//
+//  Created by user on 11.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import "CellController.h"
+
+@interface CellController ()
+
+@end
+
+@implementation CellController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
